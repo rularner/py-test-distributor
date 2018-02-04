18dca2c58fa6c9d004230303fede4cfa8d9cc117
Rusty Larner <rularner@imvu.com>
2018-02-04 05:27:39 +0000

    Testing out tests...

--- client/tests/test_simple.py
+++ client/tests/test_simple.py
@@ -4,8 +4,8 @@ import requests_mock
 import json
 
 
+@requests_mock.mock()
 class IntegrationTests(TestCase):
-    @requests_mock.mock()
     def test_client_iterates_through_tests(self, mock_requests):
         mock_requests.post('http://localhost:8080/')
         mock_requests.get('http://localhost:8080/',
@@ -20,3 +20,64 @@ class IntegrationTests(TestCase):
         testRun.set_test_list(*test_list)
         for test in testRun.test_list():
             assert test.name == test_list.pop(0)
+
+    def test_client_reports_failures(self, mock_requests):
+        mock_requests.post('http://localhost:8080/')
+        mock_requests.get('http://localhost:8080/',
+                          [
+                              {'text': json.dumps({'testName': 'a'})},
+                              {'text': json.dumps({'tests': False})},
+                          ])
+        mock_requests.post('http://localhost:8080/',
+                           {
+                               'state': 'fail',
+                               'duration': 5,
+                               'reason': 'anything goes here',
+                           })
+        test_list = ['a']
+        testRun = client.TestRun('http://localhost:8080/', 'host')
+        testRun.set_test_list(*test_list)
+        test = testRun.test_list()[0]
+        assert test.name == test_list.pop(0)
+        test.fail(duration=5, reason='anything goes here')
+
+    def test_client_reports_success(self, mock_requests):
+        mock_requests.post('http://localhost:8080/')
+        mock_requests.get('http://localhost:8080/',
+                          [
+                              {'text': json.dumps({'testName': 'a'})},
+                              {'text': json.dumps({'tests': False})},
+                          ])
+        mock_requests.post('http://localhost:8080/',
+                           {
+                               'state': 'success',
+                               'duration': 5,
+                               'reason': 'anything goes here',
+                           })
+        test_list = ['a']
+        testRun = client.TestRun('http://localhost:8080/', 'host')
+        testRun.set_test_list(*test_list)
+        test = testRun.test_list()[0]
+        assert test.name == test_list.pop(0)
+        test.success(duration=5, reason='anything goes here')
+
+    def test_client_rerequests_if_asked(self, mock_requests):
+        mock_requests.post('http://localhost:8080/')
+        mock_requests.get('http://localhost:8080/',
+                          [
+                              {'text': json.dumps({'testName': 'a'})},
+                              {'text': json.dumps({'testName': 'a'})},
+                              {'text': json.dumps({'tests': False})},
+                          ])
+        mock_requests.post('http://localhost:8080/',
+                           {
+                               'state': 'fail',
+                               'duration': 5,
+                               'reason': 'anything goes here',
+                           })
+        test_list = ['a']
+        testRun = client.TestRun('http://localhost:8080/', 'host')
+        testRun.set_test_list(*test_list)
+        test = testRun.test_list()[0]
+        assert test.name == test_list.pop(0)
+        test.fail(duration=5, reason='anything goes here')
